09ac0eddd3fb35c1be7f7ada7e7028babf851f61
Joseph Antony Pierre Avila Foure - Tismart <extjpierre@belcorp.biz>
2024-10-24 11:59:02 -0500

    add navbar in layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
         {children}
         <Footer />
       </body>
